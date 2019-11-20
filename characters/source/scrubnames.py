30d226fda445b96b86a4e541c4e68c8198d3cd1a
CJ Nguyen <cjnguyen.99@gmail.com>
2019-11-20 02:41:15 -0600

    Edited scrubnames.py for table use

--- characters/source/scrubnames.py
+++ characters/source/scrubnames.py
@@ -1,8 +1,18 @@
 import xlrd
 import os
 
-xfile = xlrd.open_workbook(os.path.join("C:/xampp/htdocs/nothw/", "characters.xlsx"), on_demand=True)
+xfile = xlrd.open_workbook(os.path.join("os.path.abspath(__file__)", "characters.xlsx"), on_demand=True)
 names = xfile.sheet_names()
 
-for name in names:
-    print("<option value=\"" + name + "\">" + name + "</a></li>")
+print("<table>")
+for i in range(0, len(names)):
+    if (i%5 == 0):
+        if (i != 0):
+            print("\t</tr>")
+        print("\t<tr>")
+    print("\t\t<td name=\"" + names[i] + "\"><a href=\"character.php?character=" + names[i] + "\"><img src=\"renders/" + names[i] + ".png\" height=\"200px\"></a></td>")
+    # if ((i+2)%10 == 0):
+    #     print("\t</tr>")
+    #     print("\t<tr>")
+print("\t</tr>")
+print("</table>")
